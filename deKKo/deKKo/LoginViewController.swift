7c8da7d5181e1d0363be315ecbb46b98ec4c1958
GitSzuKai <GitSzuKai@users.noreply.github.com>
2017-04-01 16:13:19 -0400

    Login and create done

--- deKKo/deKKo/LoginViewController.swift
+++ deKKo/deKKo/LoginViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 import GoogleSignIn
 import Google
 import FBSDKLoginKit
-class LoginViewController: UIViewController,GIDSignInUIDelegate,GIDSignInDelegate
-{
+import Parse
+import MBProgressHUD
 
+class LoginViewController: UIViewController,GIDSignInUIDelegate,GIDSignInDelegate,UITextFieldDelegate
+{
+   
+    
+    @IBOutlet weak var googleSigninBtn: GIDSignInButton!
+    
+    @IBOutlet weak var passWord: UITextField!
+    @IBOutlet weak var userName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         var configureError: NSError?
@@ -20,11 +29,46 @@ class LoginViewController: UIViewController,GIDSignInUIDelegate,GIDSignInDelegat
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().clientID = "963253939831-hod6842oqnllcnvhrmn7s56q7nn2baan.apps.googleusercontent.com"
-
-        // Do any additional setup after loading the view.
-        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+        
+        self.passWord.delegate = self
+        
+       // googleSigninBtn.style = GIDSignInButtonStyle.standard
+        
+              //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if (userName.text == "" || passWord.text == ""){
+            let alertA = UIAlertController(title: "Error", message: "Username and Password cannot be empty", preferredStyle: .alert)
+            let okC = UIAlertAction(title: "Error", style: .default, handler: nil)
+            userName.becomeFirstResponder()
+            alertA.addAction(okC)
+            self.present(alertA, animated: true, completion: nil)
+        }
+        performLogin()
+        return true
+    }
+    
+    func performLogin() {
+        PFUser.logInWithUsername(inBackground: userName.text!, password: passWord.text!) { (user: PFUser?, error: Error?) in
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+            if user != nil{
+                print("Login")
+                MBProgressHUD.hide(for: self.view, animated: true)
+                let mainView = UIStoryboard(name: "mainView", bundle: nil)
+                let vc = mainView.instantiateViewController(withIdentifier: "mainViewNavigation")
+                self.present(vc, animated: true, completion: {})
+            }else{
+                print("Error failed")
+                print(error?.localizedDescription ?? 0)
+                let alertc = UIAlertController(title: "Error", message: "Login error please login again", preferredStyle: .alert)
+                let okC = UIAlertAction(title: "Error", style: .default, handler: nil)
+                alertc.addAction(okC)
+                self.present(alertc, animated: true, completion: nil)
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -69,6 +113,8 @@ class LoginViewController: UIViewController,GIDSignInUIDelegate,GIDSignInDelegat
         
     }
 
+
+
     @IBAction func loginWithFB(_ sender: Any)
     {
         let loginManager = FBSDKLoginManager()
@@ -95,7 +141,10 @@ class LoginViewController: UIViewController,GIDSignInUIDelegate,GIDSignInDelegat
 
     }
     
-
+    @IBAction func onTapCreateNewUser(_ sender: Any) {
+    
+    }
+    
     /*
     // MARK: - Navigation
 
